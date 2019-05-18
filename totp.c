a4b69bad66c0035c5a2cc3630996c88c32f1f977
Michael Santos <michael.santos@gmail.com>
2019-05-18 09:15:31 -0400

    comments: fix formatting

--- totp.c
+++ totp.c
@@ -65,29 +65,33 @@
 #endif
 
 static const int8_t base32_vals[256] = {
-    //    This map cheats and interprets:
-    //       - the numeral zero as the letter "O" as in oscar
-    //       - the numeral one as the letter "L" as in lima
-    //       - the numeral eight as the letter "B" as in bravo
-    // 00  01  02  03  04  05  06  07  08  09  0A  0B  0C  0D  0E  0F
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 0x00
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 0x10
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 0x20
-    14, 11, 26, 27, 28, 29, 30, 31, 1,  -1, -1, -1, -1, 0,  -1, -1, // 0x30
-    -1, 0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, // 0x40
-    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, // 0x50
-    -1, 0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, // 0x60
-    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, -1, -1, -1, -1, // 0x70
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 0x80
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 0x90
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 0xA0
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 0xB0
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 0xC0
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 0xD0
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 0xE0
-    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, // 0xF0
+    /*
+         This map cheats and interprets:
+            - the numeral zero as the letter "O" as in oscar
+            - the numeral one as the letter "L" as in lima
+            - the numeral eight as the letter "B" as in bravo
+
+    00  01  02  03  04  05  06  07  08  09  0A  0B  0C  0D  0E  0F
+
+    */
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* 0x00 */
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* 0x10 */
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* 0x20 */
+    14, 11, 26, 27, 28, 29, 30, 31, 1,  -1, -1, -1, -1, 0,  -1, -1, /* 0x30 */
+    -1, 0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, /* 0x40 */
+    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1, /* 0x50 */
+    -1, 0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 13, 14, /* 0x60 */
+    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, -1, -1, -1, -1, /* 0x70 */
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* 0x80 */
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* 0x90 */
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* 0xA0 */
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* 0xB0 */
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* 0xC0 */
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* 0xD0 */
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* 0xE0 */
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, /* 0xF0 */
 };
-// static const char * base32_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567=";
+/* static const char * base32_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567="; */
 
 #ifdef SYS_EXIT_IS_FUN
 static noreturn void sys_exit(int status);
@@ -100,16 +104,16 @@ int main(int argc, char *argv[]) {
   size_t len;
   size_t keylen;
   uint32_t endianness;
-  time_t t0;  // Unix time offset to start counting time step
-  time_t now; // Unix time
-  uint64_t x; // step in seconds
-  uint64_t t; // number of steps
+  time_t t0;  /* Unix time offset to start counting time step */
+  time_t now; /* Unix time */
+  uint64_t x; /* step in seconds */
+  uint64_t t; /* number of steps */
   uint64_t offset;
   uint8_t hmac_result[20] = {0};
   size_t hmac_result_len;
   uint32_t bin_code;
   uint32_t totp;
-  uint8_t *k; // user's secret key
+  uint8_t *k; /* user's secret key */
   char buf[8] = {0};
   uint8_t keybuf[32] = {0};
   char *p;
@@ -156,7 +160,7 @@ int main(int argc, char *argv[]) {
 
   len = strlen((char *)k);
 
-  // validates base32 key
+  /* validates base32 key */
   if (((len & 0xF) != 0) && ((len & 0xF) != 8)) {
     fprintf(stderr, "%s: invalid base32 secret\n", argv[0]);
     SYS_EXIT(1);
@@ -195,57 +199,58 @@ int main(int argc, char *argv[]) {
     };
   };
 
-  // decodes base32 secret key
+  /* decodes base32 secret key */
   keylen = 0;
   for (pos = 0; pos <= (len - 8); pos += 8) {
-    // MSB is Most Significant Bits  (0x80 == 10000000 ~= MSB)
-    // MB is middle bits             (0x7E == 01111110 ~= MB)
-    // LSB is Least Significant Bits (0x01 == 00000001 ~= LSB)
+    /* MSB is Most Significant Bits  (0x80 == 10000000 ~= MSB)
+     * MB is middle bits             (0x7E == 01111110 ~= MB)
+     * LSB is Least Significant Bits (0x01 == 00000001 ~= LSB)
+     */
 
-    // byte 0
-    k[keylen + 0] = (base32_vals[k[pos + 0]] << 3) & 0xF8;  // 5 MSB
-    k[keylen + 0] |= (base32_vals[k[pos + 1]] >> 2) & 0x07; // 3 LSB
+    /* byte 0 */
+    k[keylen + 0] = (base32_vals[k[pos + 0]] << 3) & 0xF8;  /* 5 MSB */
+    k[keylen + 0] |= (base32_vals[k[pos + 1]] >> 2) & 0x07; /* 3 LSB */
     if (k[pos + 2] == '=') {
       keylen += 1;
       break;
     };
 
-    // byte 1
-    k[keylen + 1] = (base32_vals[k[pos + 1]] << 6) & 0xC0;  // 2 MSB
-    k[keylen + 1] |= (base32_vals[k[pos + 2]] << 1) & 0x3E; // 5  MB
-    k[keylen + 1] |= (base32_vals[k[pos + 3]] >> 4) & 0x01; // 1 LSB
+    /* byte 1 */
+    k[keylen + 1] = (base32_vals[k[pos + 1]] << 6) & 0xC0;  /* 2 MSB */
+    k[keylen + 1] |= (base32_vals[k[pos + 2]] << 1) & 0x3E; /* 5  MB */
+    k[keylen + 1] |= (base32_vals[k[pos + 3]] >> 4) & 0x01; /* 1 LSB */
     if (k[pos + 4] == '=') {
       keylen += 2;
       break;
     };
 
-    // byte 2
-    k[keylen + 2] = (base32_vals[k[pos + 3]] << 4) & 0xF0;  // 4 MSB
-    k[keylen + 2] |= (base32_vals[k[pos + 4]] >> 1) & 0x0F; // 4 LSB
+    /* byte 2 */
+    k[keylen + 2] = (base32_vals[k[pos + 3]] << 4) & 0xF0;  /* 4 MSB */
+    k[keylen + 2] |= (base32_vals[k[pos + 4]] >> 1) & 0x0F; /* 4 LSB */
     if (k[pos + 5] == '=') {
       keylen += 3;
       break;
     };
 
-    // byte 3
-    k[keylen + 3] = (base32_vals[k[pos + 4]] << 7) & 0x80;  // 1 MSB
-    k[keylen + 3] |= (base32_vals[k[pos + 5]] << 2) & 0x7C; // 5  MB
-    k[keylen + 3] |= (base32_vals[k[pos + 6]] >> 3) & 0x03; // 2 LSB
+    /* byte 3 */
+    k[keylen + 3] = (base32_vals[k[pos + 4]] << 7) & 0x80;  /* 1 MSB */
+    k[keylen + 3] |= (base32_vals[k[pos + 5]] << 2) & 0x7C; /* 5  MB */
+    k[keylen + 3] |= (base32_vals[k[pos + 6]] >> 3) & 0x03; /* 2 LSB */
     if (k[pos + 7] == '=') {
       keylen += 4;
       break;
     };
 
-    // byte 4
-    k[keylen + 4] = (base32_vals[k[pos + 6]] << 5) & 0xE0;  // 3 MSB
-    k[keylen + 4] |= (base32_vals[k[pos + 7]] >> 0) & 0x1F; // 5 LSB
+    /* byte 4 */
+    k[keylen + 4] = (base32_vals[k[pos + 6]] << 5) & 0xE0;  /* 3 MSB */
+    k[keylen + 4] |= (base32_vals[k[pos + 7]] >> 0) & 0x1F; /* 5 LSB */
     keylen += 5;
   };
   k[keylen] = 0;
 
   t = (now + t0) / x;
 
-  // converts T to big endian if system is little endian
+  /* converts T to big endian if system is little endian */
   endianness = 0xdeadbeef;
   if ((*(const uint8_t *)&endianness) == 0xef) {
     t = ((t & 0x00000000ffffffff) << 32) | ((t & 0xffffffff00000000) >> 32);
@@ -253,19 +258,19 @@ int main(int argc, char *argv[]) {
     t = ((t & 0x00ff00ff00ff00ff) << 8) | ((t & 0xff00ff00ff00ff00) >> 8);
   };
 
-  // determines hash
+  /* determines hash */
   hmac_result_len = sizeof(hmac_result);
   hmac_sha1(k, keylen, (const unsigned char *)&t, sizeof(t), hmac_result,
             &hmac_result_len);
 
-  // dynamically truncates hash
+  /* dynamically truncates hash */
   offset = hmac_result[19] & 0x0f;
   bin_code = (hmac_result[offset] & 0x7f) << 24 |
              (hmac_result[offset + 1] & 0xff) << 16 |
              (hmac_result[offset + 2] & 0xff) << 8 |
              (hmac_result[offset + 3] & 0xff);
 
-  // truncates code to 6 digits
+  /* truncates code to 6 digits */
   totp = bin_code % 1000000;
 
   snprintf(buf, sizeof(buf), "%06u\n", totp);
